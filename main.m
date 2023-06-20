1adbd8c97fc1fb79489bc400dca48fd8c194d66c
krajoff <t0k@bk.ru>
2023-06-20 09:42:23 +0300

    Modify main.m and delete some pit

--- main.m
+++ main.m
@@ -2,7 +2,7 @@ close all; clear; clc;
 [sf, cf] = paths;
 
 [id, gentype] = imDatFile('input_2.dat');
-id = covert2struct(id);
+id = covert2struct(id); 
 joinStructByCell({id; cf}, sf);
 
 pull(sf);
