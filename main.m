74a61d2e3772910bff9802d8956ab0cfc57fb667
krajoff <iioz@ya.ru>
2023-11-23 20:58:32 +0500

    Add m-file

--- main.m
+++ main.m
@@ -6,9 +6,9 @@ close all; clear; clc;
 % genType is a type of hydro-generator
 % id is an initial data from a dat-file 
 [id, genType] = readData('input_435_190_10.m', sf, cf);
-%[id, genType] = readData('input_670_170-44.dat', sf, cf);
-%[id, genType] = readData('input_820_175-80.dat', sf, cf);
-%[id, genType] = readData('input_1285_275-42.dat', sf, cf);
+%[id, genType] = readData('input_670_170_44.dat', sf, cf);
+%[id, genType] = readData('input_820_175_80.dat', sf, cf);
+%[id, genType] = readData('input_1285_275_42.dat', sf, cf);
 
 
 % Whole calculation modules
--- util/convert/readData.m
+++ util/convert/readData.m
@@ -1,10 +1,13 @@
 % Read initial FORTRAN77-files (aka dat-files) and m-files 
 function [id, genType] = readData(name, sf, cf)
     [~, nfile, ext] = fileparts(name);
-    if (strcmp(ext, '.dat'))
+    switch ext
+        case '.dat'
+    %if (strcmp(ext, '.dat'))
         [id, genType] = imDatFile(name);
         id = covert2struct(id);
-    elseif (strcmp(ext, '.m'))
+    %elseif (strcmp(ext, '.m'))
+        case '.m'
         [id, genType] = eval(nfile);
     end
     joinStructByCell({id; cf}, sf);
