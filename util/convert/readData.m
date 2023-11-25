8796d1c7f81fde90aa079e00fb5b5b8244573574
krajoff <iioz@ya.ru>
2023-11-25 13:35:25 +0500

    Fix some bugs

--- util/convert/readData.m
+++ util/convert/readData.m
@@ -1,12 +1,12 @@
 % Read initial FORTRAN77-files (aka dat-files) and m-files 
 function [id, genType] = readData(name, sf, cf)
-    [~, nfile, ext] = fileparts(name);
+    [~, ~, ext] = fileparts(name);
     switch ext
         case '.dat'
             [id, genType] = imDatFile(name);
             id = covert2struct(id);
         case '.m'
-            [id, genType] = eval(nfile);
+            run(name);
         case '.xls'
             [id, genType] = imXLSFile(name);
     end
