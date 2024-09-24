20b784f9994d1f6c40b5577d7b9e0dd6ab0e7f7b
krajoff <t0k@bk.ru>
2024-09-24 09:19:17 +0300

    Fix bug in ZUBTAB

--- util/calculation/zubtab.m
+++ util/calculation/zubtab.m
@@ -3,27 +3,28 @@
 % awt - current mds; bz - current induction
 
 function awt = zubtab(h,b,bz,nuzl)
-    if bz-b(1) > 0
+    if (bz-b(1)) <= 0
         tgal = b(1)/h(1);
         awt = bz/tgal;
         return; 
-    end
+    else
 
-    % Normal interpolation
-    for j = 1:nuzl-1
-        if bz >= b(j) && bz <= b(j+1)
-            jp = j;
-            tgal = (b(jp+1)-b(jp))/(h(jp+1)-h(jp));
-            db = bz-b(jp);
-            dh = db/tgal;
-            awt = h(jp)+dh;
-            return;
+        % Normal interpolation
+        for j = 1:nuzl-1
+            if bz >= b(j) && bz <= b(j+1)
+                jp = j;
+                tgal = (b(jp+1)-b(jp))/(h(jp+1)-h(jp));
+                db = bz-b(jp);
+                dh = db/tgal;
+                awt = h(jp)+dh;
+                return;
+            end
         end
+        
+        % Interpolation(for B.gt.B(nuzl))
+        tgal = (b(nuzl)-b(nuzl-1))/(h(nuzl)-h(nuzl-1));
+        db = bz-b(nuzl);
+        dh = db/tgal;
+        awt = h(nuzl)+dh;
     end
-
-    % Interpolation(for B.gt.B(nuzl))
-    tgal = (b(nuzl)-b(nuzl-1))/(h(nuzl)-h(nuzl-1));
-    db = bz-b(nuzl);
-    dh = db/tgal;
-    awt = h(nuzl)+dh;
 end
