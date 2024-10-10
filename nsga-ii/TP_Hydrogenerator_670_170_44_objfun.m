3d79ff80281eca50bc70d75cdd631d18899103ed
krajoff <t0k@bk.ru>
2024-10-10 16:20:48 +0300

    Increase calculation by 6 times

--- nsga-ii/TP_Hydrogenerator_670_170_44_objfun.m
+++ nsga-ii/TP_Hydrogenerator_670_170_44_objfun.m
@@ -3,11 +3,11 @@ function [y, cons] = TP_Hydrogenerator_670_170_44_objfun(x)
     cons = [0,0];
 
     [sf, cf] = paths;
-    [id, genType] = readData('input_670_170_44.m');
+    [id, ~] = readData('input_670_170_44.m');
     id.da = x(1); id.di = x(2); id.elt = x(3);
     joinStructByCell({id; cf}, sf);
     
-    pull(sf, genType);
+    calculation(sf);
     struct = open(sf); 
     y(1) = 1/struct.kpdd(12);
     y(2) = struct.gs;
