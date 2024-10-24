ab49ea23b35fef0012d4a3a3c464aa2411ca0eb4
krajoff <t0k@bk.ru>
2024-10-24 12:53:48 +0300

    Add constrains to 965/160-56

--- nsga-ii/TP_Hydrogenerator_965_160_56.m
+++ nsga-ii/TP_Hydrogenerator_965_160_56.m
@@ -1,21 +1,20 @@
 %*************************************************************************
-%   Test Problem : 'Hydrogenerator 670/170-44'
+%   Test Problem : 'Hydrogenerator 965/160-56'
 %*************************************************************************
 paths;
 options = nsgaopt();                    % create default options structure
-options.popsize = 100;                  % populaion size
-options.maxGen  = 10;                   % max generation
+options.popsize = 5;                   % populaion size
+options.maxGen  = 10;                    % max generation
 
 options.numObj = 2;                     % number of objectives
 options.numVar = 3;                     % number of design variables
-options.numCons = 0;                    % number of constraints
+options.numCons = 1;                    % number of constraints
 
-% lower bound
-options.lb = [9650-20 9000-20 1600-20];
+options.lb = [9650-20 9000-20 1600-20]; % lower bound
+options.ub = [9650+20 9000+20 1600+20]; % upper bound
+options.nameObj = {'Efficiency', 'StatorMass'}; 
 
-% upper bound
-options.ub = [9650+20 9000+20 1600+20]; 
 options.vartype = [2 2 2];
 options.objfun = @TP_Hydrogenerator_965_160_56_objfun;     
-options.plotInterval = 5;                        
+options.plotInterval = 5; 
 result = nsga2(options);               
--- nsga-ii/TP_Hydrogenerator_965_160_56_objfun.m
+++ nsga-ii/TP_Hydrogenerator_965_160_56_objfun.m
@@ -2,7 +2,7 @@ function [y, cons] = TP_Hydrogenerator_965_160_56_objfun(x)
 
     y = [0,0];
     cons = [0,0];
-
+        
     [id,~] = readData('input_965_160_56.dat');
     commonStruct = load('data\commondata.mat');
     id.da = x(1); id.di = x(2); id.elt = x(3);
@@ -12,4 +12,13 @@ function [y, cons] = TP_Hydrogenerator_965_160_56_objfun(x)
     y(1) = 1/mainStruct.kpdd(12);
     y(2) = mainStruct.gs;
     
+    if(mainStruct.tetfe > 44 || mainStruct.tetcu > 52)
+        cons(1) = mainStruct.tetfe;
+        cons(2) = mainStruct.tetcu;
+        fprintf("Bad: x1 = %d, x2 = %d, x3 = %d, " + ...
+                "КПД = %d, Масса статора = %d, " + ...
+                "tetfe = %d,  tetcu = %d \n",  x(1),x(2),x(3), ...
+                y(1), y(2), cons(1), cons(2));
+    end
+         
 end
